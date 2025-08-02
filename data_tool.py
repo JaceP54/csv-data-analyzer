49b7882e900a3546b2fe2c01b7a76266787cca4b
Jace Pennycuff <pennycuffjace@gmail.com>
2025-08-02 10:35:50 -0400

    UPDATE: Able to export to xlsx files.

--- data_tool.py
+++ data_tool.py
@@ -5,19 +5,16 @@ lb = "==========================================================================
 df = pd.read_csv('expenses.csv')
 
 by_date = df.sort_values(by="Date", ascending=True)
-
-Descriptions = df.sort_values(by="Description", ascending=True)
-
+by_descriptions = df.sort_values(by="Description", ascending=True)
 sorted_df = df.sort_values(by="Amount", ascending=False)
 
 total_spent = df["Amount"].sum().round(2)
 
 by_date.to_csv('daily_summary.csv', index=False)
-Descriptions.to_csv('categpry_summary.csv', index=False)
+by_descriptions.to_csv('categpry_summary.csv', index=False)
 
-"""print(lb)
-print(sorted_df[["Description", "Amount"]])
-print(lb)
-print(by_date[["Date", "Amount"]])
-print(lb)
-print(total_spent)"""
+
+by_date.to_excel('daily_summary.xlsx', index=False)
+by_descriptions.to_excel('categpry_summary.xlsx', index=False)
+
+print(total_spent)
