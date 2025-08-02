3aeb0d42fe8fab6b849bd6eccf554ad7917542d2
Jace Pennycuff <pennycuffjace@gmail.com>
2025-08-02 10:41:08 -0400

    Final Product

--- data_tool.py
+++ data_tool.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import csv
 
-lb = "==============================================================================="
 df = pd.read_csv('expenses.csv')
-
 by_date = df.sort_values(by="Date", ascending=True)
 by_descriptions = df.sort_values(by="Description", ascending=True)
 sorted_df = df.sort_values(by="Amount", ascending=False)
+daily_summary = df.groupby("Date")["Amount"].sum().reset_index()
+category_summary = df.groupby("Description")["Amount"].sum().reset_index()
 
 total_spent = df["Amount"].sum().round(2)
 
 by_date.to_csv('daily_summary.csv', index=False)
-by_descriptions.to_csv('categpry_summary.csv', index=False)
+by_descriptions.to_csv('category_summary.csv', index=False)
 
 
 by_date.to_excel('daily_summary.xlsx', index=False)
-by_descriptions.to_excel('categpry_summary.xlsx', index=False)
+by_descriptions.to_excel('category_summary.xlsx', index=False)
+
+with pd.ExcelWriter("expense_report.xlsx") as writer:
+    daily_summary.to_excel(writer, sheet_name="By Date", index=False)
+    category_summary.to_excel(writer, sheet_name="By Category", index=False)
 
 print(total_spent)
